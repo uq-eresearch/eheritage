823ff748828007bb44b2f83a67816f56123dc81f
Damien Ayers <damien@omad.net>
2014-05-01 15:43:31 +1000

    prepare for using flask-assets for managing css/js assets

--- eheritage/__init__.py
+++ eheritage/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
 from eheritage.utils import IterableAwareEncoder
+from flask.ext.assets import Environment, Bundle
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+
+# When I'm ready to fix this with bower and stuff
+# look at http://adambard.com/blog/fresh-flask-setup/
+
+environment = Environment(app)
+js = Bundle('app.js',
+    filters='jsmin', output='gen/packed.js')
+environment.register('js_all', js)
+
+scss = Bundle('*.scss', filters='scss', output='gen/scss.css')
+css = Bundle('style.css', scss,
+    filters='pyscss,cssmin', output='gen/all.css')
 
 app.json_encoder = IterableAwareEncoder
 
